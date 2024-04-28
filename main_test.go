0a3df4885c0822fd1611e0dbf1b1900c86964546
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-04-28 16:40:40 -0300

    ci: update tests

--- main_test.go
+++ main_test.go
@@ -7,41 +7,29 @@ import (
 )
 
 func TestCalculateInvestment(t *testing.T) {
-	tests := []struct {
-		initial       float64
-		monthly       float64
-		profitability float64
-		years         float64
-		expected      float64
-	}{
-		{10000.0, 500.0, 5.0, 10, calculateInvestment(10000.0, 500.0, 5.0, 10)},
-		{20000.0, 1000.0, 5.0, 10, calculateInvestment(20000.0, 1000.0, 5.0, 10)},
-		{10000.0, 500.0, 10.0, 10, calculateInvestment(10000.0, 500.0, 10.0, 10)},
-		{10000.0, 500.0, 5.0, 20, calculateInvestment(10000.0, 500.0, 5.0, 20)},
-		{0.0, 500.0, 5.0, 10, calculateInvestment(0.0, 500.0, 5.0, 10)},
-		{10000.0, 0.0, 5.0, 10, calculateInvestment(10000.0, 0.0, 5.0, 10)},
-		{10000.0, 500.0, 0.0, 10, calculateInvestment(10000.0, 500.0, 0.0, 10)},
-		{10000.0, 500.0, 5.0, 0, calculateInvestment(10000.0, 500.0, 5.0, 0)},
-	}
+	initial := 10000.0   // R$ 10.000 inicial
+	monthly := 500.0     // R$ 500 por mês
+	profitability := 5.0 // 5% ao ano
+	years := 10          // por 10 anos
 
-	for _, test := range tests {
-		result := calculateInvestment(test.initial, test.monthly, test.profitability, test.years)
-		if result < test.expected-1 || result > test.expected+1 {
-			t.Errorf("Para initial=%.2f, monthly=%.2f, profitability=%.2f, years=%.2f: esperado ~%.2f, obtido %.2f",
-				test.initial, test.monthly, test.profitability, test.years, test.expected, result)
-		}
+	expectValue := 94111.23
+
+	result := calculateInvestment(initial, monthly, profitability, float64(years))
+
+	if result < expectValue-1 || result > expectValue+1 {
+		t.Errorf("Resultado incorreto: esperado ~%.2f, obtido %.2f", expectValue, result)
 	}
 }
 
 func TestMain(t *testing.T) {
 	cmd := exec.Command("go", "run", ".")
-	cmd.Stdin = strings.NewReader("1000\n200\n0.9\n5\n")
+	cmd.Stdin = strings.NewReader("1000\n200\n10\n5\n")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Expected no error, but got '%s'", err)
 	}
 
-	expected := "Após 5 anos, o valor do investimento será de R$ 13.315,40\n"
+	expected := "Após 5 anos, o valor do investimento será de R$ 17.132,72\n"
 	if !strings.Contains(string(out), expected) {
 		t.Errorf("Expected '%s' but got '%s'", expected, string(out))
 	}

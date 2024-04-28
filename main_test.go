ad914a20d8c67511003bfc73e7a01d7eedcbd9e3
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-04-28 16:20:46 -0300

    test: update tests

--- main_test.go
+++ main_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"os/exec"
+	"strings"
 	"testing"
 )
 
-// Testa a função calculaInvestimento
 func TestCalculateInvestment(t *testing.T) {
-	// Testa um cenário com valores conhecidos
 	initial := 10000.0   // R$ 10.000 inicial
 	monthly := 500.0     // R$ 500 por mês
 	profitability := 5.0 // 5% ao ano
@@ -14,11 +14,23 @@ func TestCalculateInvestment(t *testing.T) {
 
 	expectValue := 94111.23
 
-	// Chama a função que estamos testando
 	result := calculateInvestment(initial, monthly, profitability, float64(years))
 
-	// Verifica se o resultado está dentro de uma margem de erro aceitável
 	if result < expectValue-1 || result > expectValue+1 {
 		t.Errorf("Resultado incorreto: esperado ~%.2f, obtido %.2f", expectValue, result)
+	}
+}
+
+func TestMain(t *testing.T) {
+	cmd := exec.Command("go", "run", ".")
+	cmd.Stdin = strings.NewReader("1000\n200\n0.9\n5\n")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Expected no error, but got '%s'", err)
+	}
+
+	expected := "Após 5 anos, o valor do investimento será de R$ 13.315,40\n"
+	if !strings.Contains(string(out), expected) {
+		t.Errorf("Expected '%s' but got '%s'", expected, string(out))
 	}
 }
